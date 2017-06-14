470c8d0fc601bb32c630f86cdade6f13346d93b6
devotion <303054578@qq.com>
2017-06-15 07:42:02 +0800

    代码整理

--- src/main/java/com/tale/init/SqliteJdbc.java
+++ src/main/java/com/tale/init/SqliteJdbc.java
@@ -1,5 +1,9 @@
 package com.tale.init;
 
+import com.blade.kit.ClassKit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
@@ -9,9 +13,6 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Sqlite 数据库操作
  * <p>
@@ -19,52 +20,48 @@ import org.slf4j.LoggerFactory;
  */
 public final class SqliteJdbc {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(SqliteJdbc.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(SqliteJdbc.class);
 
-	private SqliteJdbc() {
-	}
+    private SqliteJdbc() {
+    }
 
-	public static final String DB_NAME = "tale.db";
-	public static String DB_PATH = SqliteJdbc.class.getClassLoader().getResource("").getPath() + DB_NAME;
-	public static String DB_SRC = "jdbc:sqlite://" + DB_PATH;
+    private static final String DB_NAME = "tale.db";
+    public static String DB_PATH = SqliteJdbc.class.getClassLoader().getResource("").getPath() + DB_NAME;
+    public static String DB_SRC = "jdbc:sqlite://" + DB_PATH;
 
-	static {
-		try {
-			Class.forName("org.sqlite.JDBC");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    static {
+        ClassKit.loadClass("org.sqlite.JDBC");
+    }
 
-	/**
-	 * 测试连接并导入数据库
-	 */
-	@SuppressWarnings("resource")
-	public static void importSql(boolean devMode) {
-		try {
-			if (devMode) {
-				DB_PATH = System.getProperty("user.dir") + "/" + DB_NAME;
-				DB_SRC = "jdbc:sqlite://" + DB_PATH;
-			}
-			Connection con = DriverManager.getConnection(DB_SRC);
-			Statement statement = con.createStatement();
-			ResultSet rs = statement
-					.executeQuery("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='t_options'");
-			int count = rs.getInt(1);
-			if (count == 0) {
-				String cp = SqliteJdbc.class.getClassLoader().getResource("").getPath();
-				InputStreamReader isr = new InputStreamReader(new FileInputStream(cp + "schema.sql"), "UTF-8");
-				String sql = new BufferedReader(isr).lines().collect(Collectors.joining("\n"));
-				statement.executeUpdate(sql);
-				LOGGER.info("initialize import database.");
-			}
-			rs.close();
-			statement.close();
-			con.close();
-			LOGGER.info("database path is: {}", DB_PATH);
-		} catch (Exception e) {
-			LOGGER.error("initialize database fail", e);
-		}
-	}
+    /**
+     * 测试连接并导入数据库
+     */
+    @SuppressWarnings("resource")
+    public static void importSql(boolean devMode) {
+        try {
+            if (devMode) {
+                DB_PATH = System.getProperty("user.dir") + "/" + DB_NAME;
+                DB_SRC = "jdbc:sqlite://" + DB_PATH;
+            }
+            Connection con = DriverManager.getConnection(DB_SRC);
+            Statement statement = con.createStatement();
+            ResultSet rs = statement
+                    .executeQuery("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='t_options'");
+            int count = rs.getInt(1);
+            if (count == 0) {
+                String cp = SqliteJdbc.class.getClassLoader().getResource("").getPath();
+                InputStreamReader isr = new InputStreamReader(new FileInputStream(cp + "schema.sql"), "UTF-8");
+                String sql = new BufferedReader(isr).lines().collect(Collectors.joining("\n"));
+                statement.executeUpdate(sql);
+                LOGGER.info("initialize import database.");
+            }
+            rs.close();
+            statement.close();
+            con.close();
+            LOGGER.info("database path is: {}", DB_PATH);
+        } catch (Exception e) {
+            LOGGER.error("initialize database fail", e);
+        }
+    }
 
 }
